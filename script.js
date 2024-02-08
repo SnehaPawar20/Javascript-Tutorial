fec970db326794f714ccfcd861ab891dda6f1b3d
sneha pawar <snehan62@gmail.com>
2024-02-08 00:28:30 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let enterdName = prompt("Enter your Full Name: ");
+
+let fullName = enterdName.trim;
+/*let length = fullName.length;*/
+
+let userName = "@" + fullName + enterdName.length;
+
+console.log(userName);
